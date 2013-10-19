31b28dca723c93548eee2a6d94a8f4535f76c90e
Nicolas Marchildon <nicolas@marchildon.net>
2013-10-18 22:06:16 -0400

    Optional taste tag, better price extraction, fix #6 (tasting notes includes temperature), remove nbsp from temperature, larger text fields.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -57,22 +57,27 @@ class SearchController < ApplicationController
       :alcool => details["Degré d'alcool"],
       :region => details["Région"],
       :millesime => '',
-      :pastille => page.xpath("//table[@class='product-description-table']/tr/td/a/img").first['src'].sub(/.*\/(..)-g_fr.png/, '\1'),
-      :degustation => page.css('#tasting/div/p').map { |node| node.content.strip }.join(' ').strip,
+      :pastille => page.xpath("//table[@class='product-description-table']/tr/td/a/img").map { |node| node['src'].sub(/.*\/(..)-g_fr.png/, '\1') }.first,
+      :degustation => page.css('.tasting-title').select { |node| node.content == "Note de dégustation" }.map { |node| node.next_element.content.strip }.join(' ').strip,
       :garde => (garde.content.strip if garde),
       :boire => (garde.content.strip.sub(/.*garder (.+) ans.*/, '\1').to_i if garde),
-      :temperature => ((temp_spacer.previous_sibling.content.strip + " " + " " + temp_spacer.next_sibling.content.strip).gsub(/\r\n?/, " ").gsub(/\s+/, ' ').sub(/De :/, '').sub(/°C À :/, ' -').strip if temp_spacer),
+      :temperature => (clean_string(temp_spacer.previous_sibling.content.strip + " " + " " + temp_spacer.next_sibling.content.strip).gsub(/\s+/, ' ').sub(/De : /, '').sub(/°C À : /, ' - ') if temp_spacer),
       :accords => page.css("span[class='recipes-name']").map { |node| node.content.strip }.join('. ').strip,
       :quantite => 1
     }
-    if (page.css('.price-rebate').first)
-      attributes[:prix] = page.at_css('.initialprice').content.strip
-      attributes[:achat] = page.at_css('.price-rebate').content.strip
+    cart = page.at_css('.product-add-to-cart-meta')
+    cart.css('.hors-ecran, .rabais-etoile').map { |node| node.remove() }
+    if (cart.at_css('.price-rebate'))
+      attributes[:prix] = cart.at_css('.initialprice').content.strip
+      attributes[:achat] = cart.at_css('.price-rebate').content.strip
     else
-      attributes[:prix] = page.at_css('.price').content.strip
+      attributes[:prix] = cart.at_css('.price').text().strip
       attributes[:achat] = ''
     end
     return Vin.new(attributes)
   end
 
+  def clean_string(input)
+    input.gsub(/\r\n?/, " ").gsub(/[[:space:]]/, ' ').gsub(/\s+/, ' ') if input
+  end
 end
