3b43203afe18c83c9f5211ff4a75c419930ed71e
Nicolas Marchildon <nicolas@marchildon.net>
2015-02-01 00:27:14 -0500

    prawn: affiche tous les vins

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -7,61 +7,73 @@ class Pdf
     label_width = 180
     col_count = 3
     row_count = 2
+    labels_per_page = col_count * row_count
     margin = 10
     box_width = label_width - 2 * margin
     box_height = 12
-    wine = wines[0]
     Prawn::Document.generate(filename) {
       dash(3, :space => 2)
       stroke_color "aaaaaa"
       self.line_width = 0.5
-      (0.upto(row_count - 1)).each { |row|
-        (0.upto(col_count - 1)).each { |col|
-          bounding_box([label_width * col, (row_count * label_height) - row * label_height], :width => label_width, :height => label_height) {
-            stroke_bounds
-            stroke {
-              r = 27.mm / 2
-              circle [label_width / 2, label_height - r - 60], r
-            }
-            font("Helvetica", :size => 10) {
-              bounding_box([margin, label_height - margin], :width => box_width, :height => label_height - 2 * margin) {
-                font("Helvetica", :style => :bold, :size => 12) {
-                  text_box wine.nom, :at => [0, label_height - margin * 2], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
+      page_index = 0
+      (1.upto(wines.length/labels_per_page)).each {
+        start_new_page unless page_index == 0
+        (0.upto(row_count - 1)).each { |row|
+          (0.upto(col_count - 1)).each { |col|
+            wine = wines[labels_per_page * page_index + ((row) * col_count) + col]
+            if wine
+              bounding_box([label_width * col, (row_count * label_height) - row * label_height], :width => label_width, :height => label_height) {
+                stroke_bounds
+                stroke {
+                  r = 27.mm / 2
+                  circle [label_width / 2, label_height - r - 60], r
                 }
-                line_no = 0
-                text_box wine.cepage, :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
-                text_box wine.region || '', :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
-                text_box wine.millesime, :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit
-                text_box wine.achat, :at => [box_width / 2 + margin / 2, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit, :align => :right
-                text_box wine.boire, :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
-                font("Helvetica", :size => 9) {
-                  text_box wine.accords, :at => [0, 120], :width => box_width + margin / 2, :height => 200, :overflow => :shrink_to_fit
+                font("Helvetica", :size => 10) {
+                  bounding_box([margin, label_height - margin], :width => box_width, :height => label_height - 2 * margin) {
+                    font("Helvetica", :style => :bold, :size => 12) {
+                      text_box wine.nom, :at => [0, label_height - margin * 2], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
+                    }
+                    line_no = 0
+                    text_box wine.cepage, :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
+                    text_box wine.region || '', :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
+                    text_box wine.millesime, :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit
+                    text_box wine.achat, :at => [box_width / 2 + margin / 2, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit, :align => :right
+                    text_box wine.boire, :at => [0, label_height - margin * 2 - box_height * line_no += 1], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
+                    font("Helvetica", :size => 9) {
+                      text_box wine.accords, :at => [0, 120], :width => box_width + margin / 2, :height => 200, :overflow => :shrink_to_fit
+                    }
+                  }
+                }
+                rotate(90, :origin => [0, 0]) {
+                  float {
+                    text_box "V2.0  © Nicolas Marchildon 2015,  © Société des alcools du Québec, Montréal, 2007, 2010",
+                      :at => [5, -2], :width => label_height, :height => 6, :overflow => :shrink_to_fit
+                  }
                 }
               }
-            }
-            rotate(90, :origin => [0, 0]) {
-              float {
-                text_box "V2.0  © Nicolas Marchildon 2015,  © Société des alcools du Québec, Montréal, 2007, 2010",
-                  :at => [5, -2], :width => label_height, :height => 6, :overflow => :shrink_to_fit
-              }
-            }
+            end
           }
         }
-      }
-      start_new_page
-      (0.upto(row_count - 1)).each { |row|
-        (0.upto(col_count - 1)).each { |col|
-          bounding_box([label_width * col, (row_count * label_height) - row * label_height], :width => label_width, :height => label_height) {
-            stroke_bounds
-            font("Helvetica", :size => 10) {
-              bounding_box([margin, label_height - margin], :width => box_width, :height => label_height - 2 * margin) {
-                font("Helvetica", :size => 9) {
-                  text_box wine.degustation, :at => [0, 120], :width => box_width + margin / 2, :height => 200, :overflow => :shrink_to_fit
+        start_new_page
+        (0.upto(row_count - 1)).each { |row|
+          (0.upto(col_count - 1)).each { |col|
+            wine = wines[labels_per_page * page_index + ((row) * col_count) + col]
+            if wine
+              bounding_box([label_width * col, (row_count * label_height) - row * label_height], :width => label_width, :height => label_height) {
+                stroke_bounds
+                font("Helvetica", :size => 10) {
+                  bounding_box([margin, label_height - margin], :width => box_width, :height => label_height - 2 * margin) {
+                    font("Helvetica", :size => 9) {
+                      text_box wine.degustation, :at => [0, 120], :width => box_width + margin / 2, :height => 200, :overflow => :shrink_to_fit
+                    }
+                  }
                 }
-              }
             }
+            end
           }
         }
+        start_new_page
+        page_index += 1
       }
     }
   end
