5684b0a3dafa7e1edcc1fb27e2a1a92a87f374d7
Nicolas Marchildon <nicolas@marchildon.net>
2019-12-23 22:36:13 -0500

    V2.1.0: Update to ruby 2.5.5 + use TTF font

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -12,6 +12,12 @@ class Pdf
     box_width = label_width - 2 * margin
     box_height = 12
     Prawn::Document.generate(filename) {
+      font_families.update(
+          "Museo_Slab" => {
+              :normal => Rails.root.join('assets/fonts/Museo-Slab-W01-100.ttf'),
+              :bold => Rails.root.join('assets/fonts/Museo_Slab_500.ttf')
+          }
+      )
       dash(3, :space => 2)
       stroke_color "aaaaaa"
       self.line_width = 0.5
@@ -33,9 +39,9 @@ class Pdf
                   r = 30.mm / 2
                   circle [label_width / 2, label_height - r - 60], r
                 }
-                font("Helvetica", :size => 9) {
+                font("Museo_Slab", :size => 9) {
                   bounding_box([margin, label_height - margin], :width => box_width, :height => label_height - 2 * margin) {
-                    font("Helvetica", :style => :bold, :size => 12) {
+                    font("Museo_Slab", :style => :bold, :size => 12) {
                       text_box wine.nom, :at => [0, label_height - margin * 2], :width => box_width, :height => box_height, :overflow => :shrink_to_fit
                     }
                     line_no = 1.0
@@ -45,14 +51,14 @@ class Pdf
                     line_no += 1
                     text_box "Millesime:", :at => [0, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height * 0.4, :overflow => :shrink_to_fit
                     line_no += 0.4
-                    font("Helvetica", :style => :bold, :size => 10) {
+                    font("Museo_Slab", :style => :bold, :size => 10) {
                       text_box wine.millesime, :at => [0, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit
                     }
                     line_no += 1
                     text_box "Achat:", :at => [0, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height * 0.4, :overflow => :shrink_to_fit
                     line_no += 0.4
                     text_box wine.date_achat, :at => [0, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit
-                    font("Helvetica", :size => 10) {
+                    font("Museo_Slab", :size => 10) {
                       text_box wine.prix, :at => [box_width / 2 + margin / 2, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit, :align => :right
                     } 
                     line_no += 1
@@ -61,14 +67,14 @@ class Pdf
                     boire = (wine.millesime && wine.boire.to_i > 0 ? (wine.millesime.to_i + wine.boire.to_i).to_s + (wine.millesime.to_i > 0 ? '' : ' ans') : (wine.millesime.nil? ? '' : 'maintenant'))
                     text_box boire, :at => [0, label_height - margin * 2 - box_height * line_no], :width => box_width / 4, :height => box_height, :overflow => :shrink_to_fit
                     line_no += 1
-                    font("Helvetica", :size => 9) {
+                    font("Museo_Slab", :size => 9) {
                       text_box wine.accords, :at => [0, 90], :width => box_width + margin / 2, :height => 200, :overflow => :shrink_to_fit
                     }
                   }
                 }
                 rotate(90, :origin => [0, 0]) {
                   float {
-                    text_box "V2.0.1  © Nicolas Marchildon 2015,  © Société des alcools du Québec, Montréal, 2007, 2010",
+                    text_box "V2.1.0  © Nicolas Marchildon 2019,  © Société des alcools du Québec, Montréal, 2007, 2010",
                       :at => [5, -2], :width => label_height, :height => 6, :overflow => :shrink_to_fit
                   }
                 }
@@ -76,7 +82,8 @@ class Pdf
                 if wine.cup
                   qr_file = "#{wine.cup}.png"
                   qr = RQRCode::QRCode.new(wine.cup, :size => 4, :level => :h)
-                  qr.to_img.resize(200, 200).save(qr_file)
+                  qr.as_png(file: qr_file, size: 200)
+                  #qr.to_img.resize(200, 200).save(qr_file)
                   image qr_file, :at => [margin, 150], :width => 15.mm
                 end
                 # Pastille
@@ -99,11 +106,11 @@ class Pdf
             if wine
               col = col_count - col - 1
               bounding_box([label_width * col, (row_count * label_height) - row * label_height], :width => label_width, :height => label_height) {
-                font("Helvetica", :size => 10) {
+                font("Museo_Slab", :size => 10) {
                   bounding_box([margin, label_height - margin], :width => box_width, :height => label_height - 2 * margin) {
                     line_no = 0
                     text_box wine.achat, :at => [box_width / 2 + margin / 2, label_height - margin * 2 - box_height * line_no], :width => box_width / 2, :height => box_height, :overflow => :shrink_to_fit, :align => :right
-                    font("Helvetica", :size => 9) {
+                    font("Museo_Slab", :size => 9) {
                       text_box wine.degustation, :at => [0, 120], :width => box_width + margin / 2, :height => 200, :overflow => :shrink_to_fit
                     }
                   }
